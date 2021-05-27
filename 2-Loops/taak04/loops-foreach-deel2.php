87c3a5d360428727003de66a2770bb5b433f2bec
Abdullah Alibaks <abdullah.alibaks@student.rocva.nl>
2021-05-27 11:31:56 +0200

    klaar

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,12 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<label for="klas">Ik zit in klas: </label>
+<select name="klas" id="klas" >
 <?php
-$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    foreach ($klassen as $value){
+        echo "<option value='srtlower($value)'>$value</option>";
+    }
+    ?>
 
-<form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
-  <input type="submit" value="Submit">
+    </select>
+    <input type="submit" value="verzend">
+
+    
+
 </form>
-
-?>
+</body>
+</html>
