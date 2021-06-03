3cd60474a2c3cb9bd308743a8e88c21ed9bedb16
Abdullah Alibaks <abdullah.alibaks@student.rocva.nl>
2021-06-03 12:39:54 +0200

    opdracht 1 af

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -8,7 +8,28 @@
 </head>
 <body>
 <form action="advanced-2.php" method="post">
-<label for="tabelrand">Tabelranddikte</label>
+    <label for="tabelrand">Tabelranddikte (px):</label>
+    <input type="text" name="tabelrand" id="tabelrand"><br>
+    <label for="celpadding">cel-padding (px):</label>
+    <input type="text" name="celpadding" id="celpadding"><br>
+    <label for="achtergrondkleur">Achtergrondkleur:</label>
+    <select name="achtergrondkleur" id="achtergrondkleur">  
+    <?php  
+    $kleuren = array("red", "blue", "green", "black", "brown");
+    foreach ($kleuren as $achtergrond){
+        echo "<option value='$achtergrond'>$achtergrond</option>";
+    }
+    ?>
+    </select><br>
+    <label for="tekstkleur">Tekstkleur:</label>
+    <select name="tekstkleur" id="tekstkleur">
+    <?php
+    foreach ($kleuren as $value){
+        echo "<option value='$value'>$value</option>";
+    } 
+    ?>
+    </select><br>
+    <input type="submit" value="verstuur">
 </form>
     
 </body>
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>advanced 2</title>
+    <style> 
+    table, td, tr, th{
+        border: <?php echo $_POST["tabelrand"]?>px solid black;
+        border-collapse: collapse;
+        padding: <?php echo $_POST["celpadding"]?>px;
+        background-color: <?php echo $_POST["achtergrondkleur"]?>;
+        color:  <?php echo $_POST["tekstkleur"]?>;
+    }
+    </style>
 </head>
-<body>
-    
+
+<table >
+<tr>
+    <th>Key</th>
+    <th>Value</th>
+</tr>
+
+<?php
+    $student = array("Naam"=>"Abdullah Alibaks",
+                     "Afkomst"=>"Nederlands/Surinaams", 
+                     "Klas"=>"0A", 
+                     "Leeftijd"=>"20", 
+                     "Woonplaats"=>"Amsterdam", 
+                     "Favoriete sport"=>"voetbal", 
+                     "Favoriete voetbalclub"=>"AFC Ajax");
+    foreach($student as $key => $value){?>
+        <tr>
+        <td><?php echo $key?></td>
+        <td><?php echo $value?></td>
+        </tr>
+        
+        <?php } ?>
+
+</table>
 </body>
 </html>
